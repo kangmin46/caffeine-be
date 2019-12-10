9edc3a09764b9be6b7a35a2b5a783f1bf0841c4a
kangmin46 <kangmin789@naver.com>
2019-12-10 15:51:52 +0900

    refactor : change Payment field

--- src/main/java/com/woowacourse/caffeine/domain/payment/Payment.java
+++ src/main/java/com/woowacourse/caffeine/domain/payment/Payment.java
@@ -3,24 +3,32 @@ package com.woowacourse.caffeine.domain.payment;
 import com.woowacourse.caffeine.domain.exception.InvalidIdException;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Payment {
+
     private static final Long INVALID_ID_OFFSET = 0L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    private long paymentId;
+    private Long paymentId;
 
-    private long customerId;
+    private Long customerId;
 
+    @Enumerated(value = EnumType.STRING)
     private PaymentWay paymentWay;
 
-    public Payment(long paymentId, long customerId, final PaymentWay paymentWay) {
+    public Payment(final Long paymentId, final Long customerId, final PaymentWay paymentWay) {
+        this.paymentId = Objects.requireNonNull(paymentId);
+        this.customerId = Objects.requireNonNull(customerId);
+
         if (paymentId <= INVALID_ID_OFFSET && customerId <= INVALID_ID_OFFSET) {
             throw new InvalidIdException();
         }
