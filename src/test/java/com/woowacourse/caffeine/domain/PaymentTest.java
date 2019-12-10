0e173c4f45bdca5dd2bbb54e95a45aa8ac00d1c9
kangmin46 <kangmin789@naver.com>
2019-12-10 15:52:24 +0900

    refactor : add testCase in PaymentTest

--- src/test/java/com/woowacourse/caffeine/domain/PaymentTest.java
+++ src/test/java/com/woowacourse/caffeine/domain/PaymentTest.java
@@ -11,6 +11,11 @@ public class PaymentTest {
 
     @Test
     void invalid_field() {
-        assertThrows(InvalidIdException.class, () -> new Payment(-1, -20, PaymentWay.CREDIT_CARD));
+        assertThrows(InvalidIdException.class, () -> new Payment(-1L, -20L, PaymentWay.CREDIT_CARD));
+    }
+
+    @Test
+    void null_field() {
+        assertThrows(NullPointerException.class, () -> new Payment(null, null, PaymentWay.CREDIT_CARD));
     }
 }
