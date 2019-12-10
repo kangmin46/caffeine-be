c48ab2299937eba22243a8e3c3bd8df842a37f83
kangmin46 <kangmin789@naver.com>
2019-12-10 14:48:35 +0900

    feat : add InvalidIdException

---
+++ src/main/java/com/woowacourse/caffeine/domain/exception/InvalidIdException.java
@@ -0,0 +1,9 @@
+package com.woowacourse.caffeine.domain.exception;
+
+public class InvalidIdException extends RuntimeException {
+    private static final String INVALID_ID_MESSAGE = "올바른 아이디가 아닙니다.";
+
+    public InvalidIdException() {
+        super(INVALID_ID_MESSAGE);
+    }
+}
