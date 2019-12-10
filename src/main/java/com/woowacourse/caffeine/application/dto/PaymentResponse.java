879dadb376d32f749667d5aea13a54415441b420
kangmin46 <kangmin789@naver.com>
2019-12-10 14:47:22 +0900

    feat : add PaymentResponse

---
+++ src/main/java/com/woowacourse/caffeine/application/dto/PaymentResponse.java
@@ -0,0 +1,25 @@
+package com.woowacourse.caffeine.application.dto;
+
+public class PaymentResponse {
+    private final Long id;
+    private final Long customerId;
+    private final String way;
+
+    public PaymentResponse(final Long id, final Long customerId, final String way) {
+        this.id = id;
+        this.customerId = customerId;
+        this.way = way;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public String getWay() {
+        return way;
+    }
+}
