beedcb158104da3e22003cc333c04e75892bc6fb
kangmin46 <kangmin789@naver.com>
2019-12-11 18:13:15 +0900

    implement subscribe

---
+++ src/main/java/com/woowacourse/caffeine/controller/SubscribeController.java
@@ -0,0 +1,28 @@
+package com.woowacourse.caffeine.controller;
+
+import com.woowacourse.caffeine.application.service.CustomerNotificationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+import static com.woowacourse.caffeine.controller.SubscribeController.V1_SUBSCRIBE;
+
+@RestController
+@RequestMapping(V1_SUBSCRIBE)
+public class SubscribeController {
+
+    public static final String V1_SUBSCRIBE = "/v1/subscribe";
+
+    private final CustomerNotificationService customerNotificationService;
+
+    public SubscribeController(final CustomerNotificationService customerNotificationService) {
+        this.customerNotificationService = customerNotificationService;
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseBodyEmitter subscribe(@PathVariable String customerId) {
+        return customerNotificationService.subscribe(customerId);
+    }
+}
