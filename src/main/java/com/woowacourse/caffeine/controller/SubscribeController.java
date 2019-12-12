16c70d3fe9e65de974dd58593b150f6f688e8f77
kangmin46 <kangmin789@naver.com>
2019-12-12 10:44:03 +0900

    implement shop notification

--- src/main/java/com/woowacourse/caffeine/controller/SubscribeController.java
+++ src/main/java/com/woowacourse/caffeine/controller/SubscribeController.java
@@ -1,6 +1,7 @@
 package com.woowacourse.caffeine.controller;
 
 import com.woowacourse.caffeine.application.service.CustomerNotificationService;
+import com.woowacourse.caffeine.application.service.ShopNotificationService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +17,20 @@ public class SubscribeController {
     public static final String V1_SUBSCRIBE = "/v1/subscribe";
 
     private final CustomerNotificationService customerNotificationService;
+    private final ShopNotificationService shopNotificationService;
 
-    public SubscribeController(final CustomerNotificationService customerNotificationService) {
+    public SubscribeController(CustomerNotificationService customerNotificationService, ShopNotificationService shopNotificationService) {
         this.customerNotificationService = customerNotificationService;
+        this.shopNotificationService = shopNotificationService;
     }
 
     @GetMapping("/{customerId}")
-    public ResponseBodyEmitter subscribe(@PathVariable String customerId) {
+    public ResponseBodyEmitter subscribeCustomer(@PathVariable String customerId) {
         return customerNotificationService.subscribe(customerId);
+    }
+
+    @GetMapping("/{shopId}")
+    public ResponseBodyEmitter subscribeOwner(@PathVariable Long shopId ) {
+        return shopNotificationService.subscribe(shopId);
     }
 }
