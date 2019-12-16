42f879b30c1befd05facce7c5b27fa1899b4df2d
kangmin46 <kangmin789@naver.com>
2019-12-16 17:53:24 +0900

    feat : implement OwnerController class

---
+++ src/main/java/com/woowacourse/caffeine/controller/OwnerController.java
@@ -0,0 +1,39 @@
+package com.woowacourse.caffeine.controller;
+
+import com.woowacourse.caffeine.application.dto.LoginRequest;
+import com.woowacourse.caffeine.application.dto.SignUpRequest;
+import com.woowacourse.caffeine.application.service.OwnerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+import static com.woowacourse.caffeine.controller.OwnerController.V1_OWNER;
+
+@RestController
+@RequestMapping(V1_OWNER)
+public class OwnerController {
+    public static final String V1_OWNER = "v1/owners";
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity signUp(@RequestBody SignUpRequest signUpRequest) {
+        ownerService.signUp(signUpRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequest loginRequest, HttpSession httpSession) {
+        String ownerId = ownerService.login(loginRequest);
+        httpSession.setAttribute("ownerId", ownerId);
+        return ResponseEntity.ok().build();
+    }
+}
