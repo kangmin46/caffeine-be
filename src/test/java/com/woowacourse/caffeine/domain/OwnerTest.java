372f0d8e42fc4dd36d6fab989470a87be64cf791
kangmin46 <kangmin789@naver.com>
2019-12-17 13:10:02 +0900

    refactor : change owner field test

--- src/test/java/com/woowacourse/caffeine/domain/OwnerTest.java
+++ src/test/java/com/woowacourse/caffeine/domain/OwnerTest.java
@@ -1,60 +1,85 @@
 package com.woowacourse.caffeine.domain;
 
-import com.woowacourse.caffeine.application.exception.InvalidNickNameException;
 import com.woowacourse.caffeine.application.exception.InvalidPasswordException;
+import com.woowacourse.caffeine.application.exception.InvalidShopAddressException;
 import com.woowacourse.caffeine.domain.exception.InvalidEmailException;
-import com.woowacourse.caffeine.domain.exception.InvalidOwnerIdException;
+import com.woowacourse.caffeine.domain.exception.InvalidShopNameException;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OwnerTest {
+
     @Test
-    void check_owner_id_korean() {
-        assertThrows(InvalidOwnerIdException.class, () -> new Owner("로비", "코나스", "kangmin@gmail.com", "P@ssWord!@"));
+    void password_encryption() {
+        Owner owner = new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin789@naver.com", "P@ssWord!@");
+        assertThat(owner.getPassword()).isNotEqualTo("P@ssWord!@");
     }
 
     @Test
-    void check_owner_id_special_character() {
-        assertThrows(InvalidOwnerIdException.class, () -> new Owner("!!@@", "코나스", "kangmin@gmail.com", "P@ssWord!@"));
+    void check_invalid_shop_name() {
+        assertThrows(InvalidShopNameException.class, () -> new Owner("!!@@@", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", "P@ssWord!@"));
     }
 
     @Test
-    void check_owner_id_blank_empty() {
-        assertThrows(InvalidOwnerIdException.class, () -> new Owner(" ", "코나스", "kangmin@gmail.com", "P@ssWord!@"));
-        assertThrows(InvalidOwnerIdException.class, () -> new Owner("", "코나스", "kangmin@gmail.com", "P@ssWord!@"));
+    void check_invalid_shop_name_over_size() {
+        String testString = getTestString(21);
+        assertThrows(InvalidShopNameException.class, () -> new Owner(testString, "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", "P@ssWord!@"));
     }
 
     @Test
-    void check_nick_name_special_character() {
-        assertThrows(InvalidNickNameException.class, () -> new Owner("kangmin", "!!@@", "kangmin@gmail.com", "P@ssWord!@"));
+    void check_shop_name_blank_empty() {
+        assertThrows(InvalidShopNameException.class, () -> new Owner(" ", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", "P@ssWord!@"));
+        assertThrows(InvalidShopNameException.class, () -> new Owner("", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", "P@ssWord!@"));
     }
 
     @Test
-    void check_nick_name_blank() {
-        assertThrows(InvalidNickNameException.class, () -> new Owner("kangmin", " ", "kangmin@gmail.com", "P@ssWord!@"));
-        assertThrows(InvalidNickNameException.class, () -> new Owner("kangmin", "", "kangmin@gmail.com", "P@ssWord!@"));
+    void check_shop_address_special_character() {
+        assertThrows(InvalidShopAddressException.class, () -> new Owner("어디야 커피 잠실점", "!!@@", "kangmin@gmail.com", "P@ssWord!@"));
+    }
+
+    @Test
+    void check_shop_address_over_size() {
+        String testString = getTestString(101);
+        assertThrows(InvalidShopNameException.class, () -> new Owner(testString, "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", "P@ssWord!@"));
+    }
+
+    @Test
+    void check_shop_address_blank() {
+        assertThrows(InvalidShopAddressException.class, () -> new Owner("어디야 커피 잠실점", " ", "kangmin@gmail.com", "P@ssWord!@"));
+        assertThrows(InvalidShopAddressException.class, () -> new Owner("어디야 커피 잠실점", "", "kangmin@gmail.com", "P@ssWord!@"));
     }
 
     @Test
     void check_email_not_special_character() {
-        assertThrows(InvalidEmailException.class, () -> new Owner("kangmin", "코나스", "kangmingmail.com", "P@ssWord!@"));
+        assertThrows(InvalidEmailException.class, () -> new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmingmail.com", "P@ssWord!@"));
     }
 
     @Test
     void check_email_blank_empty() {
-        assertThrows(InvalidEmailException.class, () -> new Owner("kangmin", "코나스", " ", "P@ssWord!@"));
-        assertThrows(InvalidEmailException.class, () -> new Owner("kangmin", "코나스", "", "P@ssWord!@"));
+        assertThrows(InvalidEmailException.class, () -> new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", " ", "P@ssWord!@"));
+        assertThrows(InvalidEmailException.class, () -> new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", "", "P@ssWord!@"));
     }
 
     @Test
     void check_password_not_special_character() {
-        assertThrows(InvalidPasswordException.class, () -> new Owner("kangmin", "코나스", "kangmin@gmail.com", "PssWord"));
+        assertThrows(InvalidPasswordException.class, () -> new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", "PssWord"));
     }
 
     @Test
     void check_password_blank_empty() {
-        assertThrows(InvalidPasswordException.class, () -> new Owner("kangmin", "코나스", "kangmin@gmail.com", " "));
-        assertThrows(InvalidPasswordException.class, () -> new Owner("kangmin", "코나스", "kangmin@gmail.com", ""));
+        assertThrows(InvalidPasswordException.class, () -> new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", " "));
+        assertThrows(InvalidPasswordException.class, () -> new Owner("어디야 커피 잠실점", "서울특별시 송파구 석촌호수로 262 (송파동)", "kangmin@gmail.com", ""));
+    }
+
+    private String getTestString(int size) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append("a");
+        }
+        return stringBuilder.toString();
     }
 }
