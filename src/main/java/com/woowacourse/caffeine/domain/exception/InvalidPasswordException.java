2bdd25adf67549c5093144c08a85d154626715a0
kangmin46 <kangmin789@naver.com>
2019-12-18 10:53:24 +0900

    refactor : change package

--- src/main/java/com/woowacourse/caffeine/domain/exception/InvalidPasswordException.java
+++ src/main/java/com/woowacourse/caffeine/domain/exception/InvalidPasswordException.java
@@ -1,4 +1,4 @@
-package com.woowacourse.caffeine.application.exception;
+package com.woowacourse.caffeine.domain.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- src/main/java/com/woowacourse/caffeine/domain/exception/InvalidShopAddressException.java
+++ src/main/java/com/woowacourse/caffeine/domain/exception/InvalidShopAddressException.java
@@ -1,5 +1,9 @@
-package com.woowacourse.caffeine.application.exception;
+package com.woowacourse.caffeine.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidShopAddressException extends RuntimeException {
     private static final String INVALID_SHOP_ADDRESS_MESSAGE = "올바른 주소를 입력해 주세요.";
 
