a887ed75c99cf97697e883ad0513c4bb190021c1
kangmin46 <kangmin789@naver.com>
2019-12-24 15:46:08 +0900

    refactor : add search method in ShopRepository

--- src/main/java/com/woowacourse/caffeine/repository/ShopRepository.java
+++ src/main/java/com/woowacourse/caffeine/repository/ShopRepository.java
@@ -3,5 +3,11 @@ package com.woowacourse.caffeine.repository;
 import com.woowacourse.caffeine.domain.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+    Optional<List<Shop>> findByNameContaining(String name);
+
+    Optional<List<Shop>> findByAddressContaining(String address);
 }
